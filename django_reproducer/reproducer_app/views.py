347263c65e1cee6951f00038c4a54fba16fd013b
Denis Untevskiy <d.untevskiy@gmail.com>
2017-08-25 15:44:00 +0200

    / invalid: model having m2m field is created with pk being set

--- django_reproducer/reproducer_app/views.py
+++ django_reproducer/reproducer_app/views.py
@@ -3,6 +3,11 @@ from __future__ import unicode_literals
 
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from mixer.backend.django import mixer
+
+from reproducer_app.models import Post
+
 
 def index(request):
+    mixer.blend(Post, pk=42)
     return HttpResponse('Hello world!')
