9758d2af6fea2b689969bbf6f8c8c96dc5ecf617
Denis Untevskiy <d.untevskiy@gmail.com>
2017-08-25 14:27:51 +0200

    / invalid: super works in the model save method

--- django_reproducer/reproducer_app/models.py
+++ django_reproducer/reproducer_app/models.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from builtins import super
 
 from django.db import models
 
-# Create your models here.
+class TestModel(models.Model):
+    i = models.IntegerField()
+
+    def save(self, **kwargs):
+        self.i = 42
+        super().save(**kwargs)
--- django_reproducer/reproducer_app/views.py
+++ django_reproducer/reproducer_app/views.py
@@ -4,5 +4,10 @@ from __future__ import unicode_literals
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from reproducer_app.models import TestModel
+
+
 def index(request):
+    tm = TestModel(i=5)
+    tm.save()
     return HttpResponse('Hello world!')
